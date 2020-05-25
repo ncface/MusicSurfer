907d1e6804faf7636061281b01b1ed538ace7b75
unknown <ncface@gmx.de>
2020-05-25 16:43:47 +0200

    added sample audio file

--- Assets/SongEngine.cs
+++ Assets/SongEngine.cs
@@ -8,12 +8,19 @@ using UnityEngine;
 public class SongEngine : MonoBehaviour
 {
     public string songFile;
+    public string songFolder;
+
+    public void Start()
+    {
+        LoadSong(songFile);
+    }
 
     public Song LoadSong(string songFileName)
     {
         string jsonString = File.ReadAllText(songFileName);
         Song song = JsonUtility.FromJson<Song>(jsonString);
 
+        song.LoadTracks(gameObject, songFolder);
         return song;
     }
 }
@@ -24,10 +31,28 @@ public class Song
     public string BackgroundTrack;
     public Chord[] Chords;
 
-    [System.Serializable]
+    public void LoadTracks(GameObject audioManager, string SongFolder)
+    {
+        foreach(Chord chord in Chords)
+        {
+            chord.loadTrack(audioManager, SongFolder);
+        }
+    }
+    
+    [Serializable]
     public struct Chord
     {
         public string Time;
         public string Track;
+
+        [NonSerialized]
+        public AudioSource audioSource;
+
+        public void loadTrack(GameObject audioManager, string SongFolder)
+        {
+            audioSource = audioManager.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+            audioSource.clip = Resources.Load(SongFolder + Track) as AudioClip;
+        }
     }
 }
