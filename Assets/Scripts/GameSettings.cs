130160db08067d09d333af3362c42885045877a5
Max <ax.seyfried@gmx.de>
2020-05-25 17:47:45 +0200

    added GameSettings functionality

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    #region Singleton
+
+    private static GameSettings _instance;
+
+    public static GameSettings Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    #endregion
+
+    public float runSpeed; // speed of the player to init the velocity
+
+    public List<GameObject> lanes; // includes all lanes where the player can run
+}
