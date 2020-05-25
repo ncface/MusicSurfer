b3f29db742af1f474969e9ff836a86e4947b16bf
unknown <ncface@gmx.de>
2020-05-25 20:41:49 +0200

    for every chord in json a object with audio is created

---
+++ Assets/Scripts/Song.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Song
+{
+    public string BackgroundTrack;
+    public Chord[] Chords;
+    
+    
+
+    public void LoadTracks(string SongFolder)
+    {
+        //chords laden und erstellen
+        for(int i = 0; i < Chords.Length; i++)
+        {
+            //load track from disc
+            Chords[i].loadTrack(SongFolder);
+        }
+    }
+
+    [Serializable]
+    public struct Chord
+    {
+        public string Time;
+        public string Track;
+        public int Lane;
+
+        [NonSerialized]
+        public AudioClip chord;
+
+        public void loadTrack(string SongFolder)
+        {
+            chord = Resources.Load<AudioClip>(SongFolder + Track);
+        }
+    }
+}
+
---
+++ Assets/Scripts/SongEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using UnityEngine;
+
+public class SongEngine : MonoBehaviour
+{
+    public string songFolder;
+    public GameObject chordPrefab;
+    public GameObject chordsParent;
+
+    public void Start()
+    {
+        Song song = LoadSong(songFolder);
+        instantiateChords(song);
+    }
+
+    public Song LoadSong(string songFolder)
+    {
+        string jsonString = File.ReadAllText("Assets/Resources/" + songFolder + "song.json");
+        Song song = JsonUtility.FromJson<Song>(jsonString);
+        
+        song.LoadTracks(songFolder);
+        return song;
+    }
+
+    public void instantiateChords(Song song)
+    {
+        foreach(Song.Chord chord in song.Chords)
+        {
+            //insantiate chord as game object
+            GameObject chordObject = Instantiate(chordPrefab, chordsParent.transform);
+
+            // add audio-clip to new audio-source
+            AudioSource audioSource = chordObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+            audioSource.clip = chord.chord;
+
+        }
+    }
+}
