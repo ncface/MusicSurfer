b7016f141e8344fdf29569458631d73065a857f3
Max <ax.seyfried@gmx.de>
2020-05-25 14:25:37 +0200

    added some stuff

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Player : MonoBehaviour
+{
+
+    [Range(-2, 2)] public float moveValue;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+
+        float newPosX = transform.position.x + moveValue * speed * Time.deltaTime;
+
+        transform.position = new Vector3(newPosX, transform.position.y, transform.position.z);
+    }
+}
